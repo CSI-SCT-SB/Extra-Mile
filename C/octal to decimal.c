53696e40af100df479aeb8ab72573bc1734063be
HARIKRISHNAN U S <108210868+HKRcodes@users.noreply.github.com>
2022-10-25 19:36:20 +0530

    Add files via upload

---
+++ C/octal to decimal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+// function prototype
+int convertDecimalToOctal(int decimalNumber);
+
+int main() {
+
+    int decimalNumber;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimalNumber);
+
+    printf("%d in decimal = %d in octal", decimalNumber, convertDecimalToOctal(decimalNumber));
+
+    return 0;
+}
+
+// function to convert decimalNumber to octal
+int convertDecimalToOctal(int decimalNumber) {
+    int octalNumber = 0, i = 1;
+
+    while (decimalNumber != 0) {
+        octalNumber += (decimalNumber % 8) * i;
+        decimalNumber /= 8;
+        i *= 10;
+    }
+
+    return octalNumber;
+}
