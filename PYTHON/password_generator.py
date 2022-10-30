8048bb6a3e3c32853ffeb5167ef74bf3ae6616f1
Mayookha <85960883+Maii347@users.noreply.github.com>
2022-10-30 13:59:06 +0530

    Add files via upload
    A simple password generator
    
    Input: Length of your password
    Output: A random password

---
+++ PYTHON/password_generator.py
@@ -0,0 +1,12 @@
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCEDFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "[]{}();*/-_"
+all = lower + upper + numbers + symbols
+
+length = int(input("Choose length of password "))
+
+password = "".join(random.sample(all, length))
+print("Password is :", password)
