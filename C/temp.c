376170abb9e91d8a47088b47f30490a1931e39da
Aswin8962 <116820198+Aswin8962@users.noreply.github.com>
2022-10-31 19:00:28 +0530

    Add files via upload

---
+++ C/temp.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+float fahrenheit, celsius;
+celsius = 24;
+fahrenheit =( (celsius*9)/5)+32;
+printf("\n\n Temperature in fahrenheit is: %f",fahrenheit);
+return (0);
