3ad31c041a3d1bfd53597cc6b97a4e2617b2eea4
jayanthmankavil <39288329+jayanthmankavil@users.noreply.github.com>
2022-10-31 12:30:10 +0530

    Add files via upload

---
+++ C++/Majority_Element.cpp
@@ -0,0 +1,58 @@
+/* Given an array A of N elements. Find the majority element in the array.
+ A majority element in an array A of size N is an element that appears more than N/2 times in the array.*/
+class Solution{
+  public:
+    /* Function to find the candidate for Majority */
+    int findCandidate(int a[], int size) 
+    { 
+        int maj_index = 0, count = 1; 
+        
+        // use linear search to compute candidate for majority element
+        for (int i = 1; i < size; i++) 
+        { 
+            
+            if (a[maj_index] == a[i]) 
+                count++; 
+            else
+                count--; 
+            if (count == 0) 
+            { 
+                maj_index = i; 
+                count = 1; 
+            } 
+        } 
+        return a[maj_index]; 
+    }
+    
+    
+    /* Function to check if the candidate 
+       occurs more than n/2 times */
+    bool isMajority(int a[], int size, int cand) 
+    { 
+        int count = 0; 
+        for (int i = 0; i < size; i++) 
+            if (a[i] == cand) 
+                count++; 
+              
+        if (count > size/2) 
+            return 1; 
+          
+        else
+            return 0; 
+    } 
+    
+    
+    /* Function to print Majority Element */
+    int majorityElement(int a[], int size)
+    {
+        /* Find the candidate for Majority*/
+       int cand = findCandidate(a, size); 
+      
+       /* Print the candidate if it is Majority*/
+        if (isMajority(a, size, cand)) 
+            return cand;
+        else
+            return -1;
+    }
+
+};
