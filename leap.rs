ac43dcb7c98e4bb36702ccb5578d9132637e444b
CSI SCT SB <116305458+CSI-SCT-SB@users.noreply.github.com>
2022-10-31 18:26:36 +0530

    Delete leap.rs

--- leap.rs
+++
@@ -1,4 +0,0 @@
-pub fn is_leap_year(year: u64) -> bool {
-"This should return if a year is either a leap one or not";
-return year % 4 == 0 && (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0));
-}
